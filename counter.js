573c34f0b5de71236c79203f5f5f242bd5cb34fb
Anna Kuznetsova <ann_v_smith@bk.ru>
2016-02-29 23:39:15 +0500

    хуже не будет -_-

--- counter.js
+++ counter.js
@@ -6,99 +6,94 @@ const http = require('http');
 const async = require('async');
 const natural = require('natural');
 natural.PorterStemmerRu.attach();
-
 const token = fs.readFileSync('./key.txt', 'utf-8');
-const words_excl = JSON.parse(fs.readFileSync('./words.json', 'utf-8'));
 const reposUrl = 'https://api.github.com/orgs/urfu-2015/repos';
-const org = 'https://api.github.com/repos/urfu-2015/';
 
 
-function getStat(method, arg, resultCallback) {
-
+function getStat(method, arg) {
     async.waterfall(
         [
-            getRepos,
-            getReadmeWords,
-            calculateStat
-        ]);
-
-    function getRepos(callback) {
-        request(getRequestOptions(reposUrl), function (err, response, body) {
-            let resp;
-            if (!err && response.statusCode === 200) {
-                try {
-                    resp = JSON.parse(body);
-                } catch (err) {
-                    callback(err, resp);
-                }
-                callback(null, resp);
-            } else {
-                callback(err, resp);
-            }
-        });
-    }
-
-    function getReadmeWords(repos, callback) {
-        let words = {};
-        const readmePaths = [];
-        repos.forEach(function (repo) {
-            const name = repo.name;
-            if (name && (name.search(/(javascript|verstka)-tasks/) !== -1)) {
-                readmePaths.push(org + name + '/readme');
-            }
-        });
-
-        async.each(
-            readmePaths,
-
-            (path, next) => {
-                request(getRequestOptions(path), function (err, response, body) {
+            function getRepos(callback) {
+                request(getRequestOptions(reposUrl), function (err, response, body) {
+                    var resp;
                     if (!err && response.statusCode === 200) {
-                        const readme = new Buffer(
-                            JSON.parse(body).content, 'base64').toString('utf-8');
-                        const readmeWords = parseText(readme);
-                        readmeWords.forEach(function (word) {
-                            let lower = word.toLowerCase();
-                            words[lower] = (words[lower] || 0) + 1;
-                        });
-                        next();
+                        resp = JSON.parse(body);
+                        callback(null, resp);
                     } else {
-                        next(err);
+                        callback(err, resp);
+                        console.log(response.statusCode);
                     }
                 });
             },
 
-            function (err) {
-                callback(err, words);
-            });
-    }
+            function getReadmeWords(repos, callback) {
+                var words = {};
+                const org = 'https://api.github.com/repos/urfu-2015/';
+                const readmePaths = [];
+                repos.forEach(function (repo) {
+                    const name = repo.name;
+                    if (name && name.indexOf('tasks') !== -1) {
+                        readmePaths.push(org + name + '/readme');
+                    }
+                });
 
-    function calculateStat(words, callback) {
-        let result;
-        words_excl.forEach(function (word) {
-            delete words[word];
-        });
-        delete words[''];
-        const wordsArr = Object.keys(words);
-        let resultObj = {};
-        wordsArr.forEach(function (word) {
-            let stem = word.stem();
-            let amount = words[word];
-            if (resultObj[stem]) {
-                resultObj[stem].words.push(word);
-                resultObj[stem].freq += amount;
-            } else {
-                resultObj[stem] = {words: [word], freq: amount};
+                async.each(
+                    readmePaths,
+
+                    function (path, next) {
+                        request(getRequestOptions(path), function (err, response, body) {
+                            if (!err && response.statusCode === 200) {
+                                const readme = new Buffer(
+                                    JSON.parse(body).content, 'base64').toString('utf-8');
+                                const readmeWords = parseText(readme);
+                                readmeWords.forEach(function (word) {
+                                    let lower = word.toLowerCase();
+                                    words[lower] = words[lower] ? ++words[lower] : 1;
+                                });
+                                next();
+                            } else {
+                                next(err);
+                            }
+                        });
+                    },
+
+                    function (err) {
+                        callback(err, words);
+                    });
+            },
+
+            function printStat(words, callback) {
+                const wordsArr = Object.keys(words);
+                const resultObj = {};
+                const resultArr = [];
+                const roots = {};
+                wordsArr.forEach(function (word) {
+                    let stem = word.stem();
+                    resultObj[stem] = resultObj[stem] ? resultObj[stem] + words[word] : words[word];
+                    if (roots[stem]) {
+                        roots[stem].push(word);
+                    } else {
+                        roots[stem] = [word];
+                    }
+                });
+                if (method === 'top') {
+                    const rootsArr = Object.keys(resultObj);
+                    rootsArr.forEach(function (r) {
+                        resultArr.push({root: r, value: resultObj[r]});
+                    });
+                    resultArr.sort(function (r1, r2) {
+                        return r2.value - r1.value;
+                    });
+                    const top = resultArr.slice(0, arg);
+                    top.forEach(function (r) {
+                        console.log(roots[r.root] + ' ' + r.value);
+                    });
+                } else if (method === 'count') {
+                    let root = arg.stem();
+                    console.log(roots[root] + ' ' + resultObj[root]);
+                }
             }
-        });
-        if (method === 'top') {
-            result = getSortedResult(resultObj).slice(0, arg);
-        } else if (method === 'count') {
-            writeObj({resultObj: resultObj});
-            result = getCount(resultObj, arg);
-        }
-        resultCallback(null, result);
-    }
+        ]);
 }
 
 function parseText(text) {
@@ -113,72 +108,10 @@ function getRequestOptions(url) {
     };
 }
 
-function writeObj(obj) {
-    fs.writeFile('./stat.json', JSON.stringify(obj), err => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log('Результаты работы сохранены на диск в файл stat.json');
-        }
-    });
-}
-
-function getSortedResult(resultObj) {
-    let resultArr = [];
-    const rootsArr = Object.keys(resultObj);
-    rootsArr.forEach(function (r) {
-        resultArr.push({words: resultObj[r].words, freq: resultObj[r].freq});
-    });
-    resultArr.sort(function (r1, r2) {
-        return r2.freq - r1.freq;
-    });
-    writeObj({resultObj: resultObj, resultArr: resultArr});
-    return resultArr;
-}
-
-function getTop(stat, n) {
-    let result;
-    if (stat.resultArr) {
-        result = stat.resultArr.slice(0, n);
-    } else {
-        result = getSortedResult(stat.resultObj).slice(0, n);
-    }
-    return result;
-}
-
-function getCount(resultObj, word) {
-    let root = word.stem();
-    return resultObj[root].freq || 0;
-}
-
-module.exports.top = (n, callback) => {
-    fs.readFile('./stat.json', 'utf-8', (err, data) => {
-        if (!err) {
-            try {
-                const stat = JSON.parse(data);
-                const result = getTop(stat, n);
-                callback(null, result);
-            } catch (err) {
-                getStat('top', n, callback);
-            }
-        } else {
-            getStat('top', n, callback);
-        }
-    });
+module.exports.top = function (n) {
+    getStat('top', n);
 };
 
-module.exports.count = (word, callback) => {
-    fs.readFile('./stat.json', 'utf-8', (err, data) => {
-        if (!err) {
-            try {
-                const stat = JSON.parse(data);
-                const result = getCount(stat.resultObj, word);
-                callback(null, result);
-            } catch (err) {
-                getStat('count', word, callback);
-            }
-        } else {
-            getStat('count', word, callback);
-        }
-    });
+module.exports.count = function (word) {
+    getStat('count', word);
 };
