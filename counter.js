9a632a861d7964c374832078180be33a3d3c7053
Dolgih Natalia <n.e.dolgih@yandex.ru>
2016-02-29 23:18:21 +0500

    top и count возвращают промис с результатаом

--- counter.js
+++ counter.js
@@ -8,171 +8,184 @@ const token = fs.readFileSync('key.txt', 'utf-8');
 var exclude = getPrepositions();
 
 function getPrepositions() {
-    var file = fs.readFileSync('exclude.txt', 'utf-8');
-    return file.split('\r\n');
+    return fs.readFileSync('exclude.txt', 'utf-8').split('\r\n');
 }
 
+module.exports.top = function (n) {
+    receiveRepoNames()
+        .then(getTaskContent)
+        .then(workDictionary)
+        .then(findSameWords)
+        .then(function (result) {
+            return result.slice(0, n);
+        }).then(prettyPrint);
+};
 
-var rootDictionary = [];
-
-async.waterfall([
-    receiveRepoNames,
-    getTaskContent,
-    workDictionary,
-    findSameWords,
-    function (data, callback) {
-        for (var key in data) {
-            if (data.hasOwnProperty(key)){
-                rootDictionary.push([key, data[key]])
-            }
-        }
-        rootDictionary.sort(function (a, b) {
-            return b[1].num  - a[1].num;
-        });
-        callback(null);
-    },
-    function (callback) {
-        module.exports.top = function (n) {
-            return rootDictionary.slice(0, n);
-        };
-
-        module.exports.count = function (word) {
+module.exports.count = function (word) {
+    receiveRepoNames()
+        .then(getTaskContent)
+        .then(workDictionary)
+        .then(findSameWords)
+        .then(function (dict) {
             var root;
-            request('http://vnutrislova.net/' + encodeURI('разбор/по-составу/' + word),
-                function (err, res, body) {
-                    if (!err && res.statusCode == 200 && body.indexOf('span class="root">') !== -1) {
-                        root = body.substring(body.indexOf('span class="root">'));
-                        root = root.substring(18, root.indexOf('<'));
-                    } else {
-                        root = word;
-                    }
-                    var result = rootDictionary.slice().filter(function (elem) {
-                        return elem[0] == root;
-                    });
-                    if (result.length == 0) {
-                        return 0;
-                    }
-                    console.log(result);
-                    return result[0][1].num;
-
-                }
-            );
-
-        };
-        callback();
-    }
-], function () {
-
-});
-
-
-function receiveRepoNames(callback) {
-    request({
-        headers: {
-            'User-Agent': 'mdf-app'
-        },
-        uri:'https://api.github.com/orgs/urfu-2015/repos?access_token=' + token},
-        function (err, res, body) {
-            if (!err && res.statusCode == 200) {
-                var reponames = getRepoNames(JSON.parse(body)).filter(
-                    function (elem) {
-                        return (elem.indexOf('task') !== -1);
+            return new Promise(function (resolve) {
+                request('http://vnutrislova.net/' + encodeURI('разбор/по-составу/' + word),
+                    function (err, res, body) {
+                        if (!err) {
+                            root = getRoot(res, body, word);
+                        }
+                        dict.filter(function (elem) {
+                            return elem[0] == root;
+                        });
+                        if (dict.length == 0) {
+                            return 0;
+                        }
+                        prettyPrint(dict);
+                        resolve(dict);
                     }
                 );
-                callback(null, reponames);
-            } else {
-                callback(err);
+            })
+        });
+};
+
+function prettyPrint(dict) {
+    for (var e of dict) {
+        console.log(e[1].num, e[0], e[1].words);
+    }
+    return dict;
+}
+
+function receiveRepoNames() {
+    return new Promise(function (resolve, reject) {
+        request({
+            headers: {
+                'User-Agent': 'mdf-app'
+            },
+            uri:'https://api.github.com/orgs/urfu-2015/repos?access_token=' + token},
+            function (err, res, body) {
+                if (!err && res.statusCode == 200) {
+                    var reponames = getRepoNames(JSON.parse(body)).filter(
+                        function (elem) {
+                            return (elem.indexOf('task') !== -1);
+                        }
+                    );
+                    console.log('repositories names get');
+                    resolve(reponames);
+                } else {
+                    reject(err);
+                }
             }
-        }
-    )
+        )
+    });
+
 }
 
 function getRepoNames(data) {
     var result = [];
-    for (var i = 0; i < data.length; i++) {
-        result.push(data[i].name)
-    }
+    data.forEach(function (repoName) {
+        result.push(repoName.name)
+    });
     return result;
 }
 
 function getTexFromPage(data) {
-    data = data.replace(/[^а-яё]|\s+/gi, ' ').toLocaleLowerCase();
-    return data;
+    return data.replace(/[^а-яё]|\s+/gi, ' ').toLocaleLowerCase();
 }
 
-function getTaskContent(reponames, cb) {
+function getTaskContent(reponames) {
     var words = [];
-    async.each(reponames,
-        function (elem, callback) {
-            request({
-                headers: {'User-Agent': 'mdf-app'},
-                uri:'https://raw.githubusercontent.com/urfu-2015/' + elem + '/master/README.md'
-            },
-                function (err, res, body) {
-                    if (!err && res.statusCode == 200) {
-                        words.push(getTexFromPage(body).split(/\s+/));
-                        callback();
-                    } else {
-                        callback(err);
+    return new Promise(function (resolve, reject) {
+        async.each(reponames,
+            function (elem, callback) {
+                request({
+                        headers: {'User-Agent': 'mdf-app'},
+                        uri:'https://raw.githubusercontent.com/urfu-2015/' + elem + '/master/README.md'
+                    },
+                    function (err, res, body) {
+                        if (!err && res.statusCode == 200) {
+                            words.push(getTexFromPage(body).split(/\s+/));
+                            callback();
+                        } else {
+                            callback(err);
+                        }
                     }
+                );
+            },
+            function (err) {
+                if (err){
+                    reject(err);
                 }
-            );
-        },
-        function (err) {
-            cb(err, words);
-        }
-    )
+                console.log('words parsed');
+                resolve(words);
+            }
+        )
+    })
 }
 
-
-
-function workDictionary(words, callback) {
+function workDictionary(words) {
     var dictionary = {};
-    var word;
-    for (var i = 0; i < words.length; i++) {
-        for (var j = 0; j< words[i].length; j++) {
-            word = words[i][j];
+    words.forEach(function (entry) {
+        entry.forEach(function (word) {
             if (word.length > 0 && exclude.indexOf(word) == -1) {
                 if (!dictionary.hasOwnProperty(word)) {
-                    dictionary[word] = 1;
+                    dictionary[word]            = 1;
                 } else {
                     dictionary[word] += 1;
                 }
             }
-        }
-    }
-    callback(null, dictionary);
-}
-
-function findSameWords(dictionary, cb) {
-    var roots = {};
-    async.forEachOf(dictionary,
-        function (num, word, callback) {
-            var root;
-            request('http://vnutrislova.net/' + encodeURI('разбор/по-составу/' + word),
-                function (err, res, body) {
-                    if (!err && res.statusCode == 200 && body.indexOf('span class="root">') !== -1) {
-                        root = body.substring(body.indexOf('span class="root">'));
-                        root = root.substring(18, root.indexOf('<'));
-                    } else {
-                        root = word;
-                    }
-                    if (roots.hasOwnProperty(root)) {
-                        roots[root].words.push(word);
-                        roots[root].num += num;
-                    } else {
-                        roots[root] = {};
-                        roots[root].words = [word];
-                        roots[root].num = num;
-                    }
-                    callback();
-
-                }
-            )
-        },
-        function (){
-            cb(null, roots);
         });
+    });
+    console.log('dictionary created');
+    return dictionary;
 }
 
+function findSameWords(dictionary) {
+    var roots = {};
+    console.log('searchig roots');
+    return new Promise(function (resolve) {
+        async.forEachOf(dictionary,
+            function (num, word, callback) {
+                var root;
+                request('http://vnutrislova.net/' + encodeURI('разбор/по-составу/' + word),
+                    function (err, res, body) {
+                        if (!err) {
+                            root = getRoot(res, body, word);
+                        }
+                        if (roots.hasOwnProperty(root)) {
+                            roots[root].words.push(word);
+                            roots[root].num += num;
+                        } else {
+                            roots[root] = {};
+                            roots[root].words = [word];
+                            roots[root].num = num;
+                        }
+                        callback();
+                    }
+                )
+            },
+            function (){
+                var dict = [];
+                for (var key in roots) {
+                    if (roots.hasOwnProperty(key)){
+                        dict.push([key, roots[key]])
+                    }
+                }
+                dict.sort(function (a, b) {
+                    return b[1].num  - a[1].num;
+                });
+                console.log('roots founded');
+                resolve(dict);
+            });
+    });
+}
 
+function getRoot(res, body, word) {
+    var root;
+    if (res.statusCode == 200) {
+        root = body.substring(body.indexOf('span class="root">'));
+        root = root.substring(18, root.indexOf('<'));
+    } else {
+        root = word;
+    }
+    return root;
+}
