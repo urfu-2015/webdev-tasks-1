09d1727301187682be0c59706bb529f961b5ac78
Natalia Aksenova <dotokoto@gmail.com>
2016-03-07 00:26:43 +0500

    Переписала с использованием async

--- counter.js
+++ counter.js
@@ -4,7 +4,6 @@ const request = require('request');
 const fs = require('fs');
 const async = require('async');
 const natural = require('natural');
-require('buffer');
 
 const GITHUB_API_URL = 'https://api.github.com';
 const GITHUB_TOKEN_FILE = 'key.txt';
@@ -13,115 +12,116 @@ const USERNAME = 'urfu-2015';
 const STOP_WORDS_FILE = 'stop.txt';
 const STOP_WORDS = fs.readFileSync(STOP_WORDS_FILE, 'utf-8').split(/\n/);
 
-var workingFuncs = 0;
-
-var wordsCount = {};
-var roots = {};
-var totalWords = 0;
-
-top(10);
-
-function getReposList(username, finalCallback) {
-    sendRequestToGithub('/users/' + username + '/repos',
-        function (err, res, body) {
-        if (!err && res.statusCode === 200) {
-            var repos = JSON.parse(body);
-            repos = repos.filter(function (json) {
-                return /(^verstka-tasks.*)|(^javascript-tasks.*)/.test(json.name);
-            })
-                .map(function (json) {
-                    return json.name;
-                });
-            repos.forEach(function (name) {
-                getRepoReadme(username, name, finalCallback);
-                workingFuncs++;
-            });
+function getReposPage(callback) {
+    sendRequestToGithub('/users/' + USERNAME + '/repos',
+    function (err, res, body) {
+        if (err) {
+            callback(new Error('Error from Github API: ' + err));
         }
+        if (res.statusCode !== 200) {
+            callback(new Error('Wrong status from Github API: ' + res.statusCode));
+        }
+        callback(err, body);
+        return body;
     });
 }
 
-function getRepoReadme(username, repoName, finalCallback) {
-    sendRequestToGithub('/repos/' + username + '/' + repoName + '/readme',
-        function (err, res, body) {
-            if (!err && res.statusCode === 200) {
-                var readme = JSON.parse(body);
-                parseText(new Buffer(readme.content, 'base64').toString('utf-8'), finalCallback);
-            }
+function getReposNames(body, callback) {
+    body = JSON.parse(body);
+    var names = body
+        .filter(function (obj) {
+            return /(^verstka-tasks.*)|(^javascript-tasks.*)/.test(obj.name);
+        })
+        .map(function (obj) {
+            return obj.name;
+        });
+    callback(null, names);
+    return names;
+}
+
+function getReposReadme(reposNames, callback) {
+    var funcs = [];
+    reposNames.forEach(function (repoName) {
+        funcs.push(getReadmeFromGithub.bind(this, repoName));
+    });
+    async.parallel(funcs,
+        function (err, readmesArr) {
+            callback(err, readmesArr);
+            return readmesArr;
         });
 }
 
-function parseText(text, finalCallback) {
-    text = text.toLowerCase().replace('ё', 'е').replace(/\w/g, '');
-    var tokenizer = new natural.AggressiveTokenizerRu();
-    var words = tokenizer.tokenize(text);
-    words.forEach(function (word) {
-        if (!wordsCount[word]) {
-            wordsCount[word] = 1;
-        } else {
-            wordsCount[word]++;
+function getReadmeFromGithub(repoName, callback) {
+    sendRequestToGithub('/repos/' + USERNAME + '/' + repoName + '/readme',
+        function (err, res, body) {
+            if (err) {
+                callback(new Error('Cannot get readme: ' + err));
+            }
+            if (res.statusCode !== 200) {
+                callback(new Error('Wrong status from Github API: ' + res.statusCode));
+            }
+            var readme = new Buffer(JSON.parse(body).content, 'base64').toString('utf-8');
+            callback(err, readme);
+            return readme;
         }
-    });
-    words = words.filter(function (word) {
-        return STOP_WORDS.indexOf(word) === -1;
-    });
-    totalWords += words.length;
-    parseWords(words, finalCallback);
+    );
 }
 
-function parseWords(words, finalCallback) {
+function parseTexts(texts, callback) {
+    var words = [];
+    texts.forEach(function (text) {
+        text = text.toLowerCase().replace('ё', 'е').replace(/\w/g, '');
+        var tokenizer = new natural.AggressiveTokenizerRu();
+        var textWords = tokenizer.tokenize(text);
+        textWords = textWords.filter(function (word) {
+            return STOP_WORDS.indexOf(word) === -1;
+        });
+        words = words.concat(textWords);
+    });
+    callback(null, words);
+}
+
+function parseWords(words, callback) {
+    var roots = {};
     words.forEach(function (word) {
         var root = natural.PorterStemmerRu.stem(word);
         if (!roots[root]) {
             roots[root] = {
                 root: root,
                 count: 1,
-                words: [word]
+                word: word
             };
         } else {
             roots[root].count++;
-            if (roots[root].words.indexOf(word) === -1) {
-                roots[root].words.push(word);
-            }
         }
     });
-    workingFuncs--;
-    if (!workingFuncs) {
-        finalCallback(null);
+    callback(null, roots);
+}
+
+function topCallback(n, error, roots) {
+    if (error) {
+        console.log('Error: ' + error);
+        return;
     }
+    var sortedRoots = [];
+    Object.keys(roots).forEach(function (key) {
+        sortedRoots.push(roots[key]);
+    });
+    sortedRoots = sortedRoots.sort(sortCount);
+    sortedRoots.slice(0, n).forEach(function (rootObj) {
+        console.log(rootObj.word + '  ' + rootObj.count);
+    });
+    return sortedRoots.slice(0, n);
 }
 
-function top(n) {
-    getReposList(USERNAME, function (err) {
-        if (err) {
-            console.log('ERROR');
-        } else {
-            var sortedRoots = [];
-            for (var key in roots) {
-                if (roots.hasOwnProperty(key)) {
-                    sortedRoots.push(roots[key]);
-                }
-            }
-            sortedRoots = sortedRoots.sort(sortCount);
-
-            for (var i = 0; i < n; i++) {
-                console.log(sortedRoots[i].words[0] + '  ' + sortedRoots[i].count);
-            }
-        }
-    });
-}
-
-function count(word) {
-    getReposList(USERNAME, function (err) {
-        if (err) {
-            console.log('Error');
-        } else {
-            if (!wordsCount[word]) {
-                console.log(word + ' 0');
-            } else {
-                console.log(word + ' ' + wordsCount[word]);
-            }
-        }
-    });
+function countCallback(word, error, roots) {
+    if (error) {
+        console.log('Error: ' + error);
+        return;
+    }
+    var root = natural.PorterStemmerRu.stem(word);
+    console.log(word + ' ' + (roots[root].count || 0));
+    return roots[root].count || 0;
 }
 
 function sortCount(a, b) {
@@ -141,7 +141,31 @@ function sendRequestToGithub(urlPart, func) {
     request(reqOptions, func);
 }
 
+function getReadmesStatistics(mode, arg) {
+    var func;
+    if (mode !== 'top' && mode !== 'count') {
+        throw (new Error('Wrong mode in getReadmesStatistics'));
+    }
+    if (mode === 'top') {
+        func = topCallback.bind(this, arg);
+    } else {
+        func = countCallback.bind(this, arg);
+    }
+    async.waterfall([
+        getReposPage,
+        getReposNames,
+        getReposReadme,
+        parseTexts,
+        parseWords
+    ], func);
+}
+function count(word) {
+    getReadmesStatistics('count', word);
+}
 
+function top(n) {
+    getReadmesStatistics('top', n);
+}
 
 module.exports = {
     count: count,
