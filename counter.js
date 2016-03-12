96a6217e88aac7fb5d6fe002d9ac36c19aee6f51
Vasilii <kuznecov_vasilii@.mail.ru>
2016-03-12 20:47:06 +0500

    исправления

--- counter.js
+++ counter.js
@@ -1,122 +1,181 @@
-const https = require('https');
-const request = require('request');
-const fs = require('fs');
+const async = require('async');
+const GitHubApi = require('github');
 const natural = require('natural');
+const config = require('./config');
 
-var wordCountIsDone = false;
-var rootCount = {};
-var wordToRoot = {};
-var rootToWord = {};
+const githubConfig = config.githubConfig;
+const stopWords = config.stopWords;
+const repoRegExp = new RegExp(githubConfig.repoRegExp);
+const wordRegExp = new RegExp(config.wordRegExp);
 
-module.exports.top = function (n) {
-    if (wordCountIsDone) {
-        showTop(n);
-        return;
-    }
-    getWordCount(function (error) {
-        if (!error) {
-            wordCountIsDone = true;
-            showTop(n);
-        }
-    });
+module.exports.top = function (n, callback) {
+    applyToStats(
+        (stats, cb) => {
+            getTop(stats, n, cb);
+        },
+        callback
+    );
 };
 
-module.exports.count = function (word) {
-    if (wordCountIsDone) {
-        showWordCount(word);
-        return;
-    }
-    getWordCount(function (error) {
-        if (!error) {
-            wordCountIsDone = true;
-            showWordCount(word);
-        }
-    });
+module.exports.count = function (word, callback) {
+    applyToStats(
+        (stats, cb) => {
+            getWordCount(stats, word, cb);
+        },
+        callback
+    );
 };
 
-function showTop(n) {
-    var keys = Object.keys(rootCount);
-    keys.sort(function (a, b) {
-        return rootCount[b] - rootCount[a];
+function applyToStats(func, callback) {
+    async.waterfall([
+        getGithub,
+        getRepos,
+        getReadmesStats,
+        mergeStats,
+        func
+    ], callback);
+}
+
+function getGithub(callback) {
+    var github = new GitHubApi({
+        version: '3.0.0'
     });
-    for (var i = 0; i < n; i++) {
-        console.log(rootToWord[keys[i]] + ' ' + rootCount[keys[i]]);
-    }
-}
 
-function showWordCount(word) {
-    var stem = natural.PorterStemmerRu.stem(word);
-    console.log(word + ' ' + rootCount[stem]);
-}
-
-var token = '';
-var urlsToHandle = 0;
-var notWord = {};
-
-function getWordCount(callback) {
-    readNotWords();
-    readToken();
-    data = fs.readFileSync('githuburls.txt');
-    var urls = data.toString().split('\r\n');
-    urlsToHandle = urls.length;
-    urls.forEach(function (url) {
-        getReadme(url, callback);
+    github.authenticate({
+        type: 'oauth',
+        token: githubConfig.token
     });
+    callback(null, github);
 }
 
-function readNotWords() {
-    var data = fs.readFileSync('notwords.txt');
-    data.toString().split('\r\n').forEach(function (word) {
-        notWord[word] = true;
-    });
-}
-
-function readToken() {
-    var data = fs.readFileSync('key.txt');
-    token = data.toString();
-}
-
-function getReadme(url, callback) {
-    var options = {
-        url: 'https://api.github.com/repos/' + url + '/readme?access_token=' + token,
-        headers: {
-            'user-agent': 'request'
-        }
-    };
-    request(options,
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                countWords((new Buffer(JSON.parse(body).content, 'base64')).toString(), callback);
-            } else {
-                urlsToHandle--;
-                if (urlsToHandle === 0) {
-                    callback();
-                }
+function getRepos(github, callback) {
+    github.repos.getFromOrg(
+        {
+            org: githubConfig.orgname
+        },
+        (error, repos) => {
+            if (error) {
+                callback(error, null);
+                return;
             }
+            var repoNames = repos.map(getRepoName).filter(repoIsMatching);
+            callback(null, github, repoNames);
         }
     );
 }
 
-function countWords(content, callback) {
-    var tokenizer = new natural.AggressiveTokenizerRu();
-    var words = tokenizer.tokenize(content);
-    words.forEach(function (word) {
-        word = word.toLowerCase();
-        if (isRussian(word) && notWord[word] !== true) {
-            var stem = natural.PorterStemmerRu.stem(word);
-            if (rootCount[stem] === undefined) {
-                rootCount[stem] = 0;
-                rootToWord[stem] = word;
+function getRepoName(repo) {
+    return repo.name;
+}
+
+function repoIsMatching(repoName) {
+    return repoRegExp.test(repoName);
+}
+
+function getReadmesStats(github, repos, callback) {
+    async.map(
+        repos,
+        (repo, cb) => {
+            getReadmeStats(github, repo, cb);
+        },
+        callback
+    );
+}
+
+function getReadmeStats(github, repo, callback) {
+    async.waterfall([
+        (cb) => {
+            getReadme(github, repo, cb);
+        },
+        countWords
+    ], callback);
+}
+
+function getReadme(github, repo, callback) {
+    github.repos.getReadme(
+        {
+            user: githubConfig.orgname,
+            repo: repo
+        },
+        (error, data) => {
+            if (error) {
+                callback(error, null);
+                return;
             }
-            rootCount[stem]++;
+            var readme = (new Buffer(data.content, 'base64')).toString();
+            callback(null, readme);
+        }
+    );
+}
+
+function countWords(readme, callback) {
+    var stats = {
+        rootCount: {},
+        rootToWord: {}
+    };
+    var tokenizer = new natural.AggressiveTokenizerRu();
+    var words = tokenizer.tokenize(readme);
+
+    words.forEach((word) => {
+        word = word.toLowerCase();
+        if (isRussian(word) && !isStopWord(word)) {
+            var root = natural.PorterStemmerRu.stem(word);
+            if (stats.rootCount[root] === undefined) {
+                stats.rootCount[root] = 0;
+                stats.rootToWord[root] = word;
+            }
+            stats.rootCount[root]++;
         }
     });
-    urlsToHandle--;
-    if (urlsToHandle === 0) {
-        callback();
-    }
+    callback(null, stats);
 }
 
 function isRussian(word) {
-    return word.match(/^[а-я\-]+$/);
+    return wordRegExp.test(word);
+}
+
+function isStopWord(word) {
+    return stopWords[word];
+}
+
+function mergeStats(statsList, callback) {
+    var mergedStats = {
+        rootCount: {},
+        rootToWord: {}
+    };
+
+    statsList.forEach((stats) => {
+        Object.keys(stats.rootCount).forEach((root) => {
+            if (mergedStats.rootCount[root] === undefined) {
+                mergedStats.rootCount[root] = 0;
+                mergedStats.rootToWord[root] = stats.rootToWord[root];
+            }
+            mergedStats.rootCount[root] += stats.rootCount[root];
+        });
+    });
+    callback(null, mergedStats);
+}
+
+function getTop(stats, n, callback) {
+    var roots = Object.keys(stats.rootCount);
+
+    roots.sort((a, b) => {
+        return stats.rootCount[b] - stats.rootCount[a];
+    });
+    var top = roots.slice(0, n).map((root, i) => {
+        return {
+            word: stats.rootToWord[root],
+            count: stats.rootCount[root]
+        };
+    });
+    callback(null, top);
+}
+
+function getWordCount(stats, word, callback) {
+    var root = natural.PorterStemmerRu.stem(word);
+
+    if (stats.rootCount[root] === undefined) {
+        callback(null, 0);
+    }
+    callback(null, stats.rootCount[root]);
 }
---
+++ test.js
@@ -0,0 +1,20 @@
+var counter = require('./counter.js');
+
+counter.top(10, (error, top) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log(top);
+});
+counter.top(3, (error, top) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log(top);
+});
+counter.count('котик', (error, count) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log(count);
+});
