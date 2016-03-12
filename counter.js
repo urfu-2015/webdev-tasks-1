d8126e91f1c7ff0967d9ec696dee9b7d8f81a652
Anna1004 <anna.khlutkova@mail.ru>
2016-03-12 20:21:55 +0500

    codestyle

--- counter.js
+++ counter.js
@@ -6,51 +6,49 @@ const fs = require('fs');
 const token = fs.readFileSync('./key.txt', 'utf-8');
 const stopWords = require('./stopWords');
 const snowball = require('snowball-stemmers');
-var stemmer = snowball.newStemmer('russian');
-const url = 'https://api.github.com/orgs/urfu-2015/repos';
-bluebird.promisifyAll(request);
-var options = {
-    url: url,
+const stemmer = snowball.newStemmer('russian');
+const urlForRepos = 'https://api.github.com/orgs/urfu-2015/repos';
+const urlForReadme = 'https://api.github.com/repos/urfu-2015/';
+const options = {
+    url: urlForRepos,
     headers: {
         'User-Agent': 'request',
         Authorization: 'token ' + token
     }
 };
-
+const reg = /verstka-tasks|javascript-tasks/;
+var allWords = [];
+bluebird.promisifyAll(request);
 function getRes() {
-    var promise = request.getAsync(options);
-    return promise
+    return request.getAsync(options)
     .then(function (response) {
-        if (response.statusCode == 200) {
-            var info = JSON.parse(response.body);
-            var promises = info.reduce(function (result, obj) {
-                if (obj.name.indexOf('verstka-tasks') != -1 ||
-                    obj.name.indexOf('javascript-tasks') != -1) {
-                    var newOptions = {
-                        url: 'https://api.github.com/repos/urfu-2015/' + obj.name + '/readme',
-                        headers: options.headers
-                    };
-                    result.push(request.getAsync(newOptions));
-                }
-                return result;
-            }, []);
-            var masterPromise = bluebird.all(promises);
-            return masterPromise;
-        }
+        if (response.statusCode !== 200) {
+            return;
+        };
+        var info = JSON.parse(response.body);
+        var promises = info.reduce(function (result, obj) {
+            if (reg.test(obj.name)) {
+                var newOptions = {
+                    url: urlForReadme + obj.name + '/readme',
+                    headers: options.headers
+                };
+                result.push(request.getAsync(newOptions));
+            }
+            return result;
+        }, []);
+        return bluebird.all(promises);
     })
     .then(function (responses) {
-        for (var i = 0; i < responses.length; i++) {
-            if (responses[i] != undefined) {
-                var info = JSON.parse(responses[i].body);
-                var task = new Buffer(info.content, 'base64').toString('utf-8');
-                getWords(task);
-            };
-        };
+        responses.forEach(function (response) {
+            var info = JSON.parse(response.body);
+            var task = new Buffer(info.content, 'base64').toString('utf-8');
+            getWords(task);
+        });
         var collection = getDict(allWords);
         return collection;
     });
 };
-var allWords = [];
+
 function getWords(task) {
     var text = task.toLowerCase().replace(/[^а-яё]/g, ' ');
     text
@@ -68,7 +66,7 @@ function ruleForFilter(word) {
 function getDict(words) {
     return words.reduce(function (result, word) {
         var stem = stemmer.stem(word);
-        if (result[stem] != undefined) {
+        if (result[stem]) {
             result[stem].number = result[stem].number + 1;
         } else {
             result[stem] = {word: word, number: 1};
@@ -78,7 +76,7 @@ function getDict(words) {
 }
 
 function getCount(word) {
-    if (typeof (word) != 'string') {
+    if (typeof (word) !== 'string') {
         return;
     };
     return getRes()
@@ -89,16 +87,17 @@ function getCount(word) {
 }
 
 function getTop(n) {
-    if (typeof (n) != 'number') {
+    if (typeof (n) !== 'number') {
         return;
     };
     return getRes()
         .then(function (data) {
             var res = Object.keys(data)
                     .map(function (item) {
-                        return {stem: item,
-                                word: data[item].word,
-                                number: data[item].number};
+                        return {
+                            stem: item,
+                            word: data[item].word,
+                            number: data[item].number};
                     })
                     .sort(function (a, b) {
                         return b.number - a.number;
